49148b752d52ff39fb04783647f745ce941e0da1
Ethan <emrigel@gmail.com>
2021-04-30 17:23:45 -0700

    First Push

---
+++ entity_factories.py
@@ -0,0 +1,60 @@
+from components.ai import HostileEnemy
+from components import consumable
+from components.fighter import Fighter
+from components.inventory import Inventory
+from entity import Actor, Item
+
+player = Actor(
+    char="@",
+    color=(255, 255, 255),
+    name="Strong Terry",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=30, defense=2, power=5),
+    inventory=Inventory(capacity=26),
+)
+
+terry = Actor(
+    char="T",
+    color=(63, 127, 63),
+    name="Terry",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=10, defense=0, power=3),
+    inventory=Inventory(capacity=0),
+)
+
+ricky = Actor(
+    char="R",
+    color=(0, 127, 0),
+    name="Ricky",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=16, defense=1, power=4),
+    inventory=Inventory(capacity=0),
+)
+
+confusion_scroll = Item(
+    char="~",
+    color=(207, 63, 255),
+    name="Stack of Money",
+    consumable=consumable.ConfusionConsumable(number_of_turns=10),
+)
+
+fireball_scroll = Item(
+    char="~",
+    color=(255, 0, 0),
+    name="Molotov",
+    consumable=consumable.FireballDamageConsumable(damage=12, radius=3),
+)
+
+health_potion = Item(
+    char="!",
+    color=(144, 75, 6),
+    name="A Cold One",
+    consumable=consumable.HealingConsumable(amount=4),
+)
+
+lightning_scroll = Item(
+    char="~",
+    color=(255, 255, 0),
+    name="Exploding Drone",
+    consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
+)
